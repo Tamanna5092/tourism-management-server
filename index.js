0237cd04d078cf377e0ede15183fc799da0f4228
Tamanna@5092 <tamannaaktermoni51@gmailcom>
2024-11-28 23:43:31 +0600

    get all country spot

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const cors = require("cors");
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 require("dotenv").config();
 const port = process.env.PORT || 5000;
 
@@ -13,7 +14,43 @@ const corsOption = {
   optionSuccessStatus: 200,
 };
 app.use(cors(corsOption));
-app.use(express.json())
+app.use(express.json()) 
+
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.abrfq.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
+});
+
+async function run() {
+  try {
+    const touristsSpotCollection = client.db('tourismManagement').collection('touristsSpot')
+
+    // get all country spots
+    app.get('/touristsSpots' , async(req, res) => {
+        const result = await touristsSpotCollection.find().toArray()
+        res.send(result)
+    })
+
+    
+
+
+    // Send a ping to confirm a successful connection
+    await client.db("admin").command({ ping: 1 });
+    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+  } finally {
+    // Ensures that the client will close when you finish/error
+  }
+}
+run().catch(console.dir);
+
+
 
 
 app.get('/', (req, res) => {
