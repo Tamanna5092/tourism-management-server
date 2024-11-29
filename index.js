1dddd5334be3c03f2551d16142f8728b2ae34d4f
Tamanna@5092 <tamannaaktermoni51@gmailcom>
2024-11-29 20:09:24 +0600

    delete tourist spot from db

--- index.js
+++ index.js
@@ -61,6 +61,14 @@ async function run() {
       res.send(result)
     })
 
+    // delete a tourist spot from db
+    app.delete('/touristsSpot/:id', async(req, res) => {
+      const id = req.params.id
+      const query = { _id: new ObjectId(id) }
+      const result = await touristsSpotCollection.deleteOne(query)
+      res.send(result)
+    })
+
 
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
